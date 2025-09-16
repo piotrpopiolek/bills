972e74121a23ca6fd1ec62a116a2c69736d55fc4
Piotr Popiołek <piotr.popiolek93@gmail.com>
2025-09-16 07:57:05 +0200

    unique=True

--- src/db/models.py
+++ src/db/models.py
@@ -19,7 +19,7 @@ class ProcessingStatus(str, enum.Enum):
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     external_id: int = Field(
-        sa_column=Column("external_id", BigInteger, index=True)
+        sa_column=Column("external_id", BigInteger, unique=True, index=True)
     )
     created_at: datetime = Field(
         default_factory=datetime.utcnow,
